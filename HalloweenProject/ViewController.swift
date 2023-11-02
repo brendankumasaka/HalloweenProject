ec1ca684afe6ae10aab424d2b7111fddb5503d0c
Brendan Kumasaka <bhk22@hawaii.edu>
2023-11-02 13:16:07 -1000

    part 1-8

--- HalloweenProject/ViewController.swift
+++ HalloweenProject/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    var categoryOneImagesData = [String]()
+    
+    override func viewDidLoad()
+    {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!)
+        categoryOneImagesData = dict!.object(forKey:"CategoryOneImages") as! [String]
+    }
+        
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+                    {
+                        tableView.deselectRow(at: indexPath, animated: true)
+                    }
+        
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+                if segue.identifier == "mySegue"
+                {
+                    let s1 = segue.destination as! secondDetailViewController
+                    let imageIndex = tableView.indexPathForSelectedRow?.row
+                    s1.imagePass = categoryOneImagesData[imageIndex!]
+                }
+            }
     }
 
 
-}
+
 
