4577fb85371135b052ac1764517ddd6c5609c8a2
Brendan Kumasaka <bhk22@hawaii.edu>
2023-11-07 13:22:08 -1000

    more coding

--- HalloweenProject/ViewController.swift
+++ HalloweenProject/ViewController.swift
@@ -38,40 +38,40 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewDidLoad()
         
         let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
-                       let dict = NSDictionary(contentsOfFile: path!)
-                       categoryOneImagesData = dict!.object(forKey:"CategoryOneImages") as! [String]
-                   }
-               
-                   
-                   @IBOutlet weak var tableView: UITableView!
-                   
-                   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-                   {
-                       return candyArray.count
-                   }
-                   
-           func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->   UITableViewCell
-           {
-                       let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-                       let text = candyArray[indexPath.row]
-                       cell.detailTextLabel?.text = ratingsArray[indexPath.row]
-                       cell.textLabel?.text = text
-                       return cell
-                   }
-               
-               func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
-                   {
-                       tableView.deselectRow(at: indexPath, animated: true)
-                   }
-
-               //Part 9
-               
-              override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-                   if segue.identifier == "mySegue"
-                   {
-                       let s1 = segue.destination as! secondDetailViewController
-                       let imageIndex = tableView.indexPathForSelectedRow?.row
-                       s1.imagePass = categoryOneImagesData[imageIndex!]
+        let dict = NSDictionary(contentsOfFile: path!)
+        categoryOneImagesData = dict!.object(forKey:"CategoryOneImages") as! [String]
+    }
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return candyArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->   UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let text = candyArray[indexPath.row]
+        cell.detailTextLabel?.text = ratingsArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //Part 9
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "mySegue"
+        {
+            let s1 = segue.destination as! secondDetailViewController
+            let imageIndex = tableView.indexPathForSelectedRow?.row
+            s1.imagePass = categoryOneImagesData[imageIndex!]
             
             
             
@@ -81,8 +81,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             let dict = NSDictionary(contentsOfFile: path!)
             categoryTwoImagesData = dict!.object(forKey:"CategoryOneImages") as! [String]
         }
-        
-        
         
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
         {
@@ -113,8 +111,45 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 
                 
                 
+                
+                
+                let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
+                let dict = NSDictionary(contentsOfFile: path!)
+                categoryThreeImagesData = dict!.object(forKey:"CategoryThreeImages") as! [String]
             }
+            
+            
+            weak var tableView: UITableView!
+            
+            func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+            {return candyArray3.count
+            }
+            
+            func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->   UITableViewCell
+            {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+                let text = candyArray3[indexPath.row]
+                cell.detailTextLabel?.text = ratingsArray3[indexPath.row]
+                cell.textLabel?.text = text
+                return cell
+            }
+            
+            func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+            {
+                tableView.deselectRow(at: indexPath, animated: true)
+            }
+            
+            //Part 9
+            
+            func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+                if segue.identifier == "mySegue"
+                {
+                    let s3 = segue.destination as! thirdDetailViewController
+                    let imageIndex = tableView.indexPathForSelectedRow?.row
+                    s3.imagePass = categoryThreeImagesData[imageIndex!]
+                }
+            }
+            
         }
-        
     }
 }
